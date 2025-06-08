83dd93b5b4fd0113d64cc10a9bbd10c720cfa32f
RetagMunjjed <retaghadeel10years>
2025-06-08 12:26:15 -0700

    Retag add

---
+++ Retag/camera.dart
@@ -0,0 +1,86 @@
+import 'package:events/screen/detailscamera.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Camera extends StatelessWidget {
+  Camera({super.key});
+
+  List cam = [
+    {"name": "Rami"},
+    {"name": "Rami"},
+    {"name": "Rami"},
+    {"name": "Rami"},
+    {"name": "Rami"},
+    {"name": "Rami"}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/cam.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            // زر الرجوع
+            SafeArea(
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back, color: Colors.white),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GridView.builder(
+                  itemCount: cam.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10.0,
+                    crossAxisSpacing: 10.0,
+                    childAspectRatio: 3.0,
+                  ),
+                  itemBuilder: (context, i) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Detailscamera(),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              cam[i]['name'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
